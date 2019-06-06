6d5a3aed01c321eb8fd2e60b21881e71aa8d4974
Jim DiMaggio <jimdimag@gmail.com>
2019-06-06 12:20:30 -0400

    added pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FavoriteDirective } from './favorite.directive';
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe
   ],
   imports: [
     BrowserModule,
